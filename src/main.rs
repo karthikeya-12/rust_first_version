e19c5fd2468fd4ec309111678fa2ec2b0920c738
Manikanta-cv <manikantar@conceptvines.com>
2026-02-21 23:05:30 +0530

    slices, tuples, random, function calls snippets.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello World!")
+    // println!("Hello World!")
+    let a = [1, 2];
+    println!("{:?}", a);
+    println!("Second element is : {}", a[0]);
+    let mut b = vec![1, 2, 3, 4, 5, 6];
+    b.push(34);
+    println!("{:?}", b);
+    let animal: &[&str] = &["Animals", "Open"];
+    println!("Animal slices are: {:?}", animal);
+    println!("{}", animal[1]);
+
+    let mut res: String = String::from("Hello, ");
+    res.push_str("World");
+    println!("{}", res);
+    let res_shadow: &str = &res[0..=5];
+    println!("{}", res_shadow);
+    {
+        let res_shadow: &str = &res[0..5];
+        println!("{}", res_shadow);
+    }
 }
